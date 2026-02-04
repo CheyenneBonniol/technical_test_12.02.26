fd90c56bf6e004299f587ed09a7ab638cbf90ad6
Cheyenne BONNIOL <cheyenne.bonniol@etu.univ-lyon1.fr>
2026-02-04 20:40:03 +0100

    FizzBuzz (sans condition d'arrêt)

---
+++ part1/main.c
@@ -0,0 +1,7 @@
+#include "part1.h"
+
+int main(){
+    fizzBuzz(3);
+    return 0;
+
+}
---
+++ part1/part1.h
@@ -0,0 +1,6 @@
+#ifndef PART1_H_INCLUDED
+#define PART1_H_INCLUDED
+
+void fizzBuzz(int number);
+
+#endif // PART1_H_INCLUDED
